1541d03ed8ea7b4cfb5065fd9778d3885be7dee1
HasanMuradBuet <1305040.hm@ugrad.cse.buet.ac.bd>
2019-10-21 18:29:06 +0600

    Commit in 21/10/2019

--- Code/TestCreation.py
+++ Code/TestCreation.py
@@ -3,22 +3,22 @@ Lower_Limit = 1
 Upper_Limit = 2
 Total_Copy  = 5
 
-file = open("reference.fasta","r")
-seq = file.read()
+Reference_File = open("reference.fasta","r")
+Test_File = open("test.fasta","w")
+Level_File = open("level.txt","w")
+seq = Reference_File.read()
 Test_Seq = ""
 Seq_Length = len(seq)
 Segment_Length = Seq_Length//Total_Copy
 for i in range(Total_Copy):
     x = random.randint(Lower_Limit,Upper_Limit)
     index = random.randint(i*Segment_Length,(i+1)*Segment_Length-x)
-    print("("+str(index)+","+str(index+x-1)+")", end = '')
-    # print(seq[index:index+x])
+    Level_File.write("("+str(index)+","+str(index+x-1)+")")
     if(i <Total_Copy-1):
         Test_Seq += seq[i*Segment_Length:index]+seq[index:index+x]+seq[index:index+x]+ seq[index+x:(i+1)*Segment_Length]
     else:
         Test_Seq += seq[i * Segment_Length:index] + seq[index:index + x] + seq[index:index + x] + seq[index + x:Seq_Length]
-
-print("")
-print(seq)
-print(Test_Seq)
-file.close()
+Test_File.write(Test_Seq)
+Reference_File.close()
+Test_File.close()
+Level_File.close()
