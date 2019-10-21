7fda46ffd3263c8c05b5605732a0bff4ae0f1f67
HasanMuradBuet <1305040.hm@ugrad.cse.buet.ac.bd>
2019-10-21 21:21:04 +0600

    Commit in mim's room on 21/10/2019

--- Code/TestCreation.py
+++ Code/TestCreation.py
@@ -2,6 +2,8 @@ import random
 Lower_Limit = 1
 Upper_Limit = 2
 Total_Copy = 5
+Lower_Number_of_Copy_Limit = 2
+Upper_Number_of_Copy_Limit = 3
 
 Reference_File = open("reference.fasta","r")
 Test_File = open("test.fasta","w")
@@ -14,10 +16,17 @@ for i in range(Total_Copy):
     x = random.randint(Lower_Limit,Upper_Limit)
     index = random.randint(i*Segment_Length,(i+1)*Segment_Length-x)
     Level_File.write("("+str(index)+","+str(index+x-1)+")")
+    number_of_copy = random.randint(Lower_Number_of_Copy_Limit,Upper_Number_of_Copy_Limit)
     if(i <Total_Copy-1):
-        Test_Seq += seq[i*Segment_Length:index]+seq[index:index+x]+seq[index:index+x]+ seq[index+x:(i+1)*Segment_Length]
+        Test_Seq += seq[i*Segment_Length:index];
+        for j in range(number_of_copy):
+            Test_Seq +=seq[index:index+x]
+        Test_Seq +=seq[index+x:(i+1)*Segment_Length]
     else:
-        Test_Seq += seq[i * Segment_Length:index] + seq[index:index + x] + seq[index:index + x] + seq[index + x:Seq_Length]
+        Test_Seq += seq[i * Segment_Length:index];
+        for j in range(number_of_copy):
+            Test_Seq += seq[index:index + x]
+        Test_Seq +=seq[index + x:Seq_Length]
 Test_File.write(Test_Seq)
 Reference_File.close()
 Test_File.close()
